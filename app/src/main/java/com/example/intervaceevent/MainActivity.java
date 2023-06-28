2f4e26aabc219f612e6c305520ed15cefc387cec
Maksy Sendiang <maksy@elektro.polimdo.ac.id>
2023-06-28 13:32:23 +0800

    Perubahan keEmpat

--- app/src/main/java/com/example/intervaceevent/MainActivity.java
+++ app/src/main/java/com/example/intervaceevent/MainActivity.java
@@ -5,31 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button btnKiri = findViewById(R.id.btnKiri);
-        btnKiri.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button Kiri diklik", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        btnKiri.setOnClickListener(this);
         Button btnKanan = findViewById(R.id.btnKanan);
-        btnKanan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Button Kanan diklik", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btnKanan.setOnClickListener(this);
+        EditText edtH = findViewById(R.id.edtHello);
+        edtH.setOnClickListener(this);
+
+//        Button btnKiri = findViewById(R.id.btnKiri);
+//        btnKiri.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Button Kiri diklik", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        Button btnKanan = findViewById(R.id.btnKanan);
+//        btnKanan.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Button Kanan diklik", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 
@@ -38,5 +45,23 @@ public class MainActivity extends AppCompatActivity {
         TextView hello = findViewById(R.id.txtHello);
         hello.setText("WELCOME KOMINFO");
         System.out.println("WELCOME KOMINFO");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnKiri:
+                Toast.makeText(MainActivity.this, "Button Kiri diklik", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnKanan:
+                Toast.makeText(MainActivity.this, "Button Kanan diklik", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.edtHello:
+                Toast.makeText(MainActivity.this, "Text diklik", Toast.LENGTH_SHORT).show();
+                break;
+                default:
+                //Toast.makeText(MainActivity.this, "Button Kanan diklik", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
