99dbd5e024797f645171a71769a65ce35dc53db6
Maksy Sendiang <maksy@elektro.polimdo.ac.id>
2023-06-28 13:00:12 +0800

    Perubahan kedua

--- app/src/main/java/com/example/intervaceevent/MainActivity.java
+++ app/src/main/java/com/example/intervaceevent/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnKiri = findViewById(R.id.btnKiri);
+        btnKiri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Kiri diklik", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnKanan = findViewById(R.id.btnKanan);
+        btnKanan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Kanan diklik", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public void clickHello(View v)
